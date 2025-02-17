905a6b2ad43be42981d83f785d02aca95f00b814
Vitalii Chernysh <vivich4omega@gmail.com>
2025-02-17 17:19:29 +0100

    test fix

--- tests/db_base.py
+++ tests/db_base.py
@@ -1,10 +1,12 @@
 import pytest
 
-from ai_service.db import get_qdrant_client
+from ai_service.db import create_qdrant_client, get_qdrant_client
 from tests.base import TestBase
 
 
 class TestDBBase(TestBase):
-    @pytest.fixture
+    @pytest.fixture(scope="function")
     def db_client(self, app):
+        with app.app_context():
+            create_qdrant_client()
         return get_qdrant_client()
--- tests/test_ml.py
+++ tests/test_ml.py
@@ -8,6 +8,6 @@ from tests.base import TestBase
 
 class TestML(TestBase):
     def test_predict_lyrics(self, raw_lyrics):
-        predictions = ml.predict_lyrics([l.lyrics for l in raw_lyrics])
+        predictions = ml.predict_lyrics(raw_lyrics)
         prediction_sums = [round(sum(astuple(p)), 5) for p in predictions]
         compare([1] * len(predictions), prediction_sums)
